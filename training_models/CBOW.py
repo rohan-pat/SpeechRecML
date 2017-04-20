3abae30a2fa1ff5f7505337ffebed510a75712aa
Rohan <rohan.patil@icloud.com>
2017-04-20 15:16:14 -0400

    Forward Prop complete
    Completed the forward propagation for continuous bag of words.

--- training_models/CBOW.py
+++ training_models/CBOW.py
@@ -22,8 +22,11 @@ class CBOW:
         self.text_dict = {}
         self.text_list = []
         self.window_size = 9
-        self.h_output = None
-        self.h_input = None
+        # hidden_layer is the hidden layer.
+        self.hidden_layer = None
+        # output_layer is the output of the hidden layer.
+        self.output_layer = None
+        self.yj = None
 
     def loadText(self):
         with open(self.text, "r") as ftext:
@@ -40,6 +43,8 @@ class CBOW:
             self.v = len(self.text_set)
         self.w0 = np.random.rand(self.v, self.n)
         self.w1 = np.random.rand(self.n, self.v)
+        self.output_layer = np.matrix(np.random.rand(1, self.v))
+        self.yj = np.matrix(np.random.rand(1, self.v))
 
     def generateInputVector(self):
         text_list = sorted(self.text_set)
@@ -47,15 +52,14 @@ class CBOW:
         for i in text_list:
             self.text_dict[i] = count
             count = count + 1
-        for i in range(8):
+        for i in range(4):
             self.text_list.insert(i, "</s>")
             self.text_list.append("</s>")
 
     def forwardProp(self):
-        total = 0;
+        total = 0
+        first = True
         print("length of text list is "+str(len(self.text_list)))
-        print(self.text_list[4-4:4+5])
-        print(self.text_list[57116-4:57116+5])
         for i in range(4, len(self.text_list)):
             text_temp = self.text_list[i-4:i+5]
             if len(text_temp) != 9:
@@ -65,15 +69,21 @@ class CBOW:
             # create list of lists to create a C X V matrix
             inner_matrix = np.matrix(np.zeros((self.window_size, self.v), dtype=float))
             count = 0
+            index = self.text_dict[self.text_list[i]]
             for item in text_temp:
-                inner_matrix[count, self.text_dict[item]] = 1
+                inner_matrix[count, index] = 1
                 count = count + 1
             sum_matrix = np.sum(inner_matrix, axis=0)
             sum_matrix = sum_matrix / self.window_size
-            self.h_output = np.dot(sum_matrix, self.w0)
-            self.h_input = np.dot(self.h_output, self.w1)
-            # calculate the exponential function for all the rows here.
-            # print(self.h_input.shape)
+            self.hidden_layer = np.dot(sum_matrix, self.w0)
+            for j in range(self.v):
+                uj = np.dot(self.hidden_layer, self.w1[:,[j]])
+                # print(uj)
+                self.output_layer[0, j] = activation_function(uj[0, 0])
+            sum_exp = np.sum(self.output_layer, axis=1)
+            # print("Shape of sum_exp = "+str(sum_exp.shape))
+            for j in range(self.v):
+                self.yj[0, j] = self.output_layer[0, j] / sum_exp[0,0]
             # then check how is the number represented utimately.
         print("Total is "+str(total))
 
