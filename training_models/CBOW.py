ded0678a3d0174813cc75b2918f1ca38b3b20eec
Rohan <rohan.patil@icloud.com>
2017-04-21 14:38:44 -0400

    Completed CBOW model
    CBOW model complete, will save weight vector with least error to DB.

--- training_models/CBOW.py
+++ training_models/CBOW.py
@@ -1,15 +1,10 @@
 """ Training continous bag of words model."""
-
+import time
 import numpy as np
 from nltk.stem.porter import PorterStemmer
-import decimal
-
-def activation_function(x):
-    # exponential function as activation
-    # using decimal to better handle floating point underflow.
-    x = decimal.Decimal(-x)
-    z = x.exp()
-    return float(round(decimal.Decimal(1) / decimal.Decimal(1 + z), 8))
+import sys
+sys.path.insert(0, '/Users/Rohan/Documents/Studies/Spring2017/ML/ProjectCode/SpeechRecML/preprocessing')
+from dbInsert import DBOperation
 
 class CBOW:
     def __init__(self):
@@ -17,7 +12,7 @@ class CBOW:
         # generating a set of unique words.
         self.text_set = None
         self.v = 0
-        self.n = 50
+        self.n = 10
         self.w0 = None
         self.text_dict = {}
         self.text_list = []
@@ -27,6 +22,9 @@ class CBOW:
         # output_layer is the output of the hidden layer.
         self.output_layer = None
         self.yj = None
+        self.tj = None
+        self.sum_exp = None
+        self.uj_output = None
 
     def loadText(self):
         with open(self.text, "r") as ftext:
@@ -41,10 +39,12 @@ class CBOW:
             self.text_set = set(self.text_list)
             print("size of text is "+str(len(self.text_set)))
             self.v = len(self.text_set)
+        print("length of text list is "+str(len(self.text_list)))
         self.w0 = np.random.rand(self.v, self.n)
         self.w1 = np.random.rand(self.n, self.v)
         self.output_layer = np.matrix(np.random.rand(1, self.v))
         self.yj = np.matrix(np.random.rand(1, self.v))
+        self.tj = np.matrix(np.random.rand(1, self.v))
 
     def generateInputVector(self):
         text_list = sorted(self.text_set)
@@ -58,15 +58,14 @@ class CBOW:
 
     def forwardProp(self):
         total = 0
-        first = True
-        print("length of text list is "+str(len(self.text_list)))
+        rcount = 0
+        # print("length of text list is "+str(len(self.text_list)))
         for i in range(4, len(self.text_list)):
             text_temp = self.text_list[i-4:i+5]
             if len(text_temp) != 9:
                 break
             total = total + 1
 
-            # create list of lists to create a C X V matrix
             inner_matrix = np.matrix(np.zeros((self.window_size, self.v), dtype=float))
             count = 0
             index = self.text_dict[self.text_list[i]]
@@ -76,19 +75,75 @@ class CBOW:
             sum_matrix = np.sum(inner_matrix, axis=0)
             sum_matrix = sum_matrix / self.window_size
             self.hidden_layer = np.dot(sum_matrix, self.w0)
+            output_list = []
             for j in range(self.v):
+                rcount = rcount + 1
                 uj = np.dot(self.hidden_layer, self.w1[:,[j]])
                 # print(uj)
-                self.output_layer[0, j] = activation_function(uj[0, 0])
-            sum_exp = np.sum(self.output_layer, axis=1)
+                output_list.append(uj[0, 0])
+            self.uj_output = np.matrix(output_list)
+            self.output_layer = np.exp(np.matrix(output_list))
+            self.sum_exp = np.sum(self.output_layer, axis=1)
             # print("Shape of sum_exp = "+str(sum_exp.shape))
-            for j in range(self.v):
-                self.yj[0, j] = self.output_layer[0, j] / sum_exp[0,0]
-            # then check how is the number represented utimately.
-        print("Total is "+str(total))
+            self.yj = self.yj / self.sum_exp[0,0]
+        #     if total % 500 == 0:
+        #         print("Count is "+str(rcount)+" ,"+str(total))
+        # print("Total is "+str(total))
+
+    def backPropogation(self):
+        total = 0
+        rcount = 0
+        learning_rate = 0.3
+        # print("length of text list is "+str(len(self.text_list)))
+        for i in range(4, len(self.text_list)):
+            text_temp = self.text_list[i-4:i+5]
+            if len(text_temp) != 9:
+                break
+            total = total + 1
+            index = self.text_dict[self.text_list[i]]
+            self.tj[0, index] = 1
+
+            for i in range(self.v):
+                rcount = rcount + 1
+                gradient1 = learning_rate * (self.yj[0,i] - self.tj[0,i]) * self.hidden_layer
+                self.w1[:,[i]] = self.w1[:,[i]] - gradient1.transpose()
+
+            EH = self.yj - self.tj
+            EH = np.multiply(EH, self.w1)
+            # print(EH.shape)
+            self.w0 = self.w0 - EH.transpose()
+            # if total % 500 == 0:
+            #     print("Count is "+str(rcount)+" ,"+str(total))
+
+    def calculateError(self):
+        total_error = 0.0
+        # print(self.uj_output.shape)
+        for i in range(self.v):
+            total_error = total_error - self.uj_output[0,i] - np.log(self.sum_exp[0,0])
+        print("Total Error is "+str(total_error))
+        return total_error
 
 if __name__ == "__main__":
     c = CBOW()
     c.loadText()
     c.generateInputVector()
-    c.forwardProp()
+    epoch = 1
+    total_error = 0
+    total_runtime = time.time()
+    d = DBOperation()
+    while epoch != 100:
+        print("---------%s-----------" %(epoch))
+        f_time = time.time()
+        c.forwardProp()
+        print("Forward Prop => %s seconds" % round((time.time() - f_time), 2))
+
+        b_time = time.time()
+        c.backPropogation()
+        print("Back Prop => %s seconds" % round((time.time() - b_time), 2))
+
+        if total_error < c.calculateError():
+            print("Lower error rate found")
+            d.storeWeightMatrix(self.w0)
+
+        epoch = epoch + 1
+    print("Total time => %s seconds" % round((time.time() - total_runtime), 2))
