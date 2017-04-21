1913612e29773be7f62b51fa12436129ca840116
Rohan <rohan.patil@icloud.com>
2017-04-20 21:12:50 -0400

    changed dbinsert

--- preprocessing/dbInsert.py
+++ preprocessing/dbInsert.py
@@ -1,25 +1,21 @@
 from pymongo import MongoClient
 import json
 
-client = MongoClient()
-db = client.test #client['test']
-coll = db['speechRec'] #db.speechRec
+class DBOperation:
+    client = MongoClient()
+    db = client.test #client['test']
+    coll = db['speechRec'] #db.speechRec
 
-print("Db Insert")
-
-print(db)
-
-def insertFeatureData(key, word, wavArr, featureArr):
+def insertFeatureData(self, key, word, wavArr, featureArr):
     data = {}
     data['word'] = word
     data['keyId'] = key
     data['wavArr'] = wavArr
     for i in range(len(featureArr)):
         data['feature'+str(i)] = featureArr[i]
+    db.inputFeatures.insert_one(data)
 
-    return data
-
-def insertRawData(keyId, text, sample):
+def insertRawData(self, keyId, text, sample):
     data = {}
     words = text.split(" ")
     wordArr = []
@@ -29,26 +25,25 @@ def insertRawData(keyId, text, sample):
     data['text'] = text
     data['keyId'] = keyId
     data['wordArr'] = wordArr
-    #jsonData = json.dumps(doc)
-    return data
-    #return jsonData
+    db.inputData.insert_one(data)
 
-sample = [];
-sampleI = [34,16,676]
-sampleLove = [41,28,303,15]
-sampleChi = [24,355,573]
-sample.append(sampleI)
-sample.append(sampleLove)
-sample.append(sampleChi)
-someText = "I LOVE CHICKEN"
-keyId = 832562621
+if __name__ == "__main__":
+    sample = [];
+    sampleI = [34,16,676]
+    sampleLove = [41,28,303,15]
+    sampleChi = [24,355,573]
+    sample.append(sampleI)
+    sample.append(sampleLove)
+    sample.append(sampleChi)
+    someText = "I LOVE CHICKEN"
+    keyId = 832562621
 
-rawJson = insertRawData(keyId, someText, sample)
+    rawJson = insertRawData(keyId, someText, sample)
 
-featureJson = insertFeatureData(keyId, "Rohan", sampleI, sample)
+    featureJson = insertFeatureData(keyId, "Rohan", sampleI, sample)
 
-# print(jsonDoc)
-print(featureJson)
+    # print(jsonDoc)
+    print(featureJson)
 
-#result = db.speechRec.insert_one(jsonDoc)
-#print(result)
+    #result = db.speechRec.insert_one(jsonDoc)
+    #print(result)
